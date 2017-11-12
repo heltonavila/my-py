c9d23eae402e877589eefc1d1729d30a7866e71a
Teo Calvo <teocalvo2@gmail.com>
2017-11-12 01:21:27 -0200

    Decision Tree Binarization
    Using decision tree method to binning numeric variables.

--- analysis/modeling.py
+++ analysis/modeling.py
@@ -1,15 +1,33 @@
 import pandas as pd
+import numpy as np
+from sklearn import tree
 
-def binariza( X ):
-    Y = pd.DataFrame()
-    categorias = list(X.unique())
-    for i in categorias:
-        Y[ X.name + " " + i ] = (X == i)
-    return Y
+class tree_bins():
 
-def binariza_colunas( df ):
-	colunas = list(df.columns)
-	df_result = pd.DataFrame()
-	for c in colunas:
-		df_result = pd.concat( [df_result, binariza( df[c] )], axis=1 )
-	return df_result
+	def __init__(self, **kwargs):
+		self.clf = tree.DecisionTreeClassifier(**kwargs)
+
+	def fit(self, X, y):
+		self.clf.fit( X, y )
+
+	def make_bins(self, X, name):
+		df = pd.DataFrame()
+		df[name] = self.clf.apply( X )
+		if X.shape[1] > 1:
+			df_dummy = pd.get_dummies( df[name])
+		else:
+			df_dummy = pd.get_dummies( df[name], prefix=name)
+		df = pd.concat( [df,df_dummy], axis=1 )
+		return df
+
+def mult_bins(df=None, columns=None, target=None, method=tree_bins, **kwargs):
+	y = df[target]
+	df_list = []
+	models_bin = []
+	for c in columns:
+		x = df[c].values.reshape(-1,1)
+		model = method( **kwargs )
+		model.fit(x , df[target] )
+		df_list.append( model.make_bins( x, name= c+"_tb" ) )
+		models_bin.append(model)
+	return pd.concat( df_list, axis=1), models_bin
