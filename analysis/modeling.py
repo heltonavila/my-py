b55b5ba2835277a414d50689c1dd03d573688e40
Teo Calvo <teocalvo2@gmail.com>
2017-11-12 23:22:04 -0200

    Reshape fix

--- analysis/modeling.py
+++ analysis/modeling.py
@@ -4,30 +4,34 @@ from sklearn import tree
 
 class tree_bins():
 
-	def __init__(self, **kwargs):
-		self.clf = tree.DecisionTreeClassifier(**kwargs)
+    def __init__(self, **kwargs):
+        self.clf = tree.DecisionTreeClassifier(**kwargs)
 
-	def fit(self, X, y):
-		self.clf.fit( X, y )
+    def fit(self, X, y):
+        try:
+            self.clf.fit( X, y )
+        except ValueError as err:
+            self.clf.fit( X.values.reshape(-1,1), y )
 
-	def make_bins(self, X, name):
-		df = pd.DataFrame()
-		df[name] = self.clf.apply( X )
-		if X.shape[1] > 1:
-			df_dummy = pd.get_dummies( df[name])
-		else:
-			df_dummy = pd.get_dummies( df[name], prefix=name)
-		df = pd.concat( [df,df_dummy], axis=1 )
-		return df
+    def make_bins(self, X, name):
+        df = pd.DataFrame()
+
+        try:
+            df[name] = self.clf.apply( X )
+        except ValueError as err:
+            df[name] = self.clf.apply( X.values.reshape(-1,1) )
+
+        df_dummy = pd.get_dummies( df[name], prefix=name)       
+        df = pd.concat( [df,df_dummy], axis=1 )
+        return df
 
 def mult_bins(df=None, columns=None, target=None, method=tree_bins, **kwargs):
-	y = df[target]
-	df_list = []
-	models_bin = []
-	for c in columns:
-		x = df[c].values.reshape(-1,1)
-		model = method( **kwargs )
-		model.fit(x , df[target] )
-		df_list.append( model.make_bins( x, name= c+"_tb" ) )
-		models_bin.append(model)
-	return pd.concat( df_list, axis=1), models_bin
+    y = df[target]
+    df_list = []
+    models_bin = []
+    for c in columns:
+        model = method( **kwargs )
+        model.fit(x , df[target] )
+        df_list.append( model.make_bins( x, name= c+"_tb" ) )
+        models_bin.append(model)
+    return pd.concat( df_list, axis=1), models_bin
